b70e22210ca25e1de6e6a88b3ca18dedd366885f
s6804062612293 <s6804062612293@email.kmutnb.ac.th>
2026-03-08 19:37:52 +0700

    Update lab10no3_2.c

--- lab10no3_2.c
+++ lab10no3_2.c
@@ -21,3 +21,22 @@ void DisplayData2(struct Books *CBook){
     printf("Book ID : %d \n",(*CBook).BookID); 
     printf("Book Title : %s \n",CBook->BookTitle); 
 } 
+
+/*
+
+ทดสอบรันโปรแกรมและเขียนผลลัพท์พร้อมอธิบายโปรแกรม
+
+void DisplayData2(struct Books *CBook) คือ ฟังก์ชันที่ชื่อ DisplayData2 
+โดยรับข้อมูลชนิด Pointer ของ Struct และไม่มี Return 
+ฟังก์ชันนี้ทำหน้าที่แสดงข้อมูลของ struct ที่รับเข้ามา
+
+พารามิเตอร์ที่รับเข้ามาคือ struct Books *CBook 
+โดยเป็นการ Passing by Address
+
+(*CBook).BookID คือ การเข้าถึงข้อมูล BookID ใน struct 
+โดยใช้ Pointer dereference แล้วใช้ . เพื่อเข้าถึงสมาชิกใน struct
+
+CBook->BookTitle คือ การใช้ -> เพื่อเข้าถึงสมาชิกใน struct ผ่าน pointer 
+ซึ่งมีความหมายเหมือนกับ (*CBook).BookTitle
+
+*/
