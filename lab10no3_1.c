eeccb856b04774a95be2a0a57ac5d855434422fa
s6804062612293 <s6804062612293@email.kmutnb.ac.th>
2026-03-08 19:36:31 +0700

    Update lab10no3_1.c

--- lab10no3_1.c
+++ lab10no3_1.c
@@ -25,3 +25,36 @@ void DisplayData(struct Books CBook){
     printf("Book Title : %s \n",CBook.BookTitle); 
 } 
 
+
+/*
+
+ทดสอบรันโปรแกรมและเขียนผลลัพธ์พร้อมอธิบายโปรแกรม
+
+ตัวอย่างการรันโปรแกรม
+
+Enter Book ID :101
+Enter Book Title :Cprogramming
+
+ผลลัพธ์
+
+Book ID : 101
+Book Title : Cprogramming
+
+
+คำอธิบายโปรแกรม
+
+void DisplayData(struct Books CBook) คือ ฟังก์ชันที่ชื่อ DisplayData
+โดยรับข้อมูลชนิด Struct และไม่มีการ return ค่า โดยฟังก์ชันนี้มีหน้าที่
+แสดงข้อมูลของ struct ที่รับเข้ามา
+
+พารามิเตอร์ที่รับเข้ามาคือ struct Books CBook ซึ่ง CBook เป็นตัวแปร
+local ภายในฟังก์ชัน โดยเป็นการส่งค่าแบบ Passing by Value
+
+struct Books เป็นโครงสร้างข้อมูลที่ใช้เก็บข้อมูลหนังสือ ประกอบด้วย
+int BookID สำหรับเก็บรหัสหนังสือ และ char BookTitle[50] สำหรับเก็บชื่อหนังสือ
+
+ในฟังก์ชัน main จะรับค่ารหัสหนังสือและชื่อหนังสือจากผู้ใช้เก็บไว้ในตัวแปร
+ComputerBook จากนั้นส่งข้อมูล ComputerBook ไปยังฟังก์ชัน DisplayData()
+เพื่อแสดงผลข้อมูล Book ID และ Book Title ทางหน้าจอ
+
+*/
